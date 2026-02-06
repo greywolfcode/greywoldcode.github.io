5b605094955aeeea7af45e8ebdfe01c82c43b93b
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2026-02-05 21:50:50 -0500

    Added serious mode

---
+++ scripts/toFunMode.js
@@ -0,0 +1,23 @@
+const pattern = /[a-zA-Z0-9]/; //define searching pattern for a-z, A-Z, and 0-9
+let currentString = ""; //define storage string
+
+function getKeyPress(event)
+{
+    if (event.key == "Backspace") //allow user to use backspace to remove typos
+    {
+        currentString = currentString.slice(0, -1);
+    }
+    else if (event.key == "Enter")
+        {
+            if (currentString == "LetsHaveFun") //go to serious mode page if string is matched
+            {
+                location.href = 'index.html';
+            }
+        }
+    else if (pattern.test(event.key) && event.key.length == 1) //only want a-z, A-Z, and 0-9 to be valid inputs
+    {
+        currentString += event.key;
+    }
+}
+
+document.addEventListener("keydown", getKeyPress) //add event listener to document
